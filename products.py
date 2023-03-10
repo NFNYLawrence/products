84bef29c10314948d656274f51fa32f377f59bea
NFNYLawrence <re08980970@gmail.com>
2023-03-10 18:03:54 +0800

    add price

--- products.py
+++ products.py
@@ -3,7 +3,15 @@ while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
+    price = input('請輸入商品價格: ')
+    np = []
+    np.append(name)
+    np.append(price)    
     products.append(np)
+
+    # np = [name, price]
+    # products.append(np)
+    
+    #products.append([name, price])
+    
 print(products)
-print(products[2])
-print(products[2][0])
